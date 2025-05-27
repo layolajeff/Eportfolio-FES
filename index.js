a08dfc67f9f0a52793f74f2523c80a7b4a0657bc
Jeffrey <layolaj@yahoo.com>
2025-05-27 10:45:11 -0700

    javascript

---
+++ index.js
@@ -0,0 +1,34 @@
+// template_1gedmlt
+
+// service_y8qwpfo
+
+// gre1zD_S_UgKC6c_M
+
+
+
+function contact (event) {
+    event.preventDefault();
+    const loading = document.querySelector ('.modal__overlay--loading')
+        const success = document.querySelector ('.modal__overlay--success')
+        loading.classList += " modal__overlay--visible";
+
+     emailjs
+      .sendForm(
+            'service_y8qwpfo',
+            'template_1gedmlt', 
+           event.target,
+            '94xlOlahhEtLLMVlh'
+        ).then(() => {
+            loading.classList.remove ("modal__overlay--visible");
+            success.classList += " modal__overlay--visible";
+        }).catch(() =>{ 
+            loading.classList.remove ("modal__overlay--visible");
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on layolajeff@gmail.com"
+            );
+        })
+}
+
+function toggleModal () {
+    
+}
