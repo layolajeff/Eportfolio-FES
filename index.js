6bd625239200a4c729bb38237787b6b13064e137
Jeffrey <layolaj@yahoo.com>
2025-05-27 12:58:37 -0700

    shapes

--- index.js
+++ index.js
@@ -4,7 +4,32 @@
 
 // 94xlOlahhEtLLMVlh--public key
 
+let isModalOpen =false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i =0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px )`
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+    
+}
 
 function contact (event) {
     event.preventDefault();
@@ -29,6 +54,12 @@ function contact (event) {
         })
 }
 
-function toggleModal () {
 
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove ("modal--open")
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
